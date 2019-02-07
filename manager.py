30e20661b6d76098f4563f504209900e3ca52147
Lucas Nauman <Lnauman1700@gmail.com>
2019-02-07 12:26:39 -0500

    added some comments to manager.py and made changes to make the user interface look a little bit better

--- manager.py
+++ manager.py
@@ -9,24 +9,31 @@ class Manager(object):
 
         file = open("todos.txt", "r")
 
+        # makes a list representation of all the lines in the txt file
         lines_list = file.readlines()
 
         # should print the info out to the terminal
         for line in lines_list:
+            # split each individual line at the separators (comma in this case)
             contents = line.split(",")
+            # print the task name out
             print(f"Task: {contents[0]}")
+            # print the task timestamp out
             print(f"Time created: {contents[1]}")
+            # tell user whether item's is_completed = True or False
             if contents[2] == "True\n":
                 print("Task Status: Complete")
             else:
                 print("Task Status: Not Yet Completed")
 
+            # added separation between items
             print("-" * 10)
 
     # should be able to add a new item to the list (database, todos.txt)
     def add_item(self, item):
         # should open up text file with append capabilities
         file = open("todos.txt", "a")
+        # write item's attributes to the file
         file.write(f"{item.job},{item.timestamp},{item.is_complete}\n")
         file.close()
 
@@ -42,21 +49,27 @@ class Manager(object):
         # then replace the False with True
         index = 0
         for line in lines_list:
+            # checks if the task name is in the line
             if job in line:
+                # split the line at its separators
                 contents = line.split(",")
+                # check that the is_completed attribute is False
                 if contents[2] == "False\n":
                     contents[2] = 'True\n'
+                    # change the list at the current index (representing the current line) to have is_completed = True
                     lines_list[index] = f"{contents[0]},{contents[1]},{contents[2]}"
+                    # escape the loop so we don't change anything else
                     break
+
             index += 1
-        # this else statement is attached to the for loop, and will activate only when the loop terminates without any lines changed
+        # this else statement is attached to the for loop, and will activate only when the loop terminates without hitting the break statement
         else:
             print("Sorry, the task was either already completed or just not found")
 
         # delete contents of file entirely
         file.truncate(0)
 
-        # rewrite the file entirely with the new, revised line
+        # rewrite the file entirely with the new, revised lines_list
         for line in lines_list:
             file.write(line)
 
--- todos.py
+++ todos.py
@@ -2,29 +2,36 @@
 import manager
 import items
 
-# the program should be able to print a list of the contents of the to-do list
-
-# should be able to add new items to the list
-
-# should be able to mark items as completed
 man = manager.Manager()
 
 what_do = input('What would you like to do?: ')
 
+# adds a new item to todos.txt
 if what_do == "add":
+
     print('What is the name of the task you\'d like to add?')
     task_name = input('> ')
     new_task = items.Item(task_name)
     man.add_item(new_task)
+
     print(f'added the task {task_name}')
+
+# gets a list of the items currently in todos.txt
 elif what_do == "list":
     man.current_list()
+
+# completes an item in the todos.txt
 elif what_do == "complete":
     print("What is the name of the task you'd like to mark as completed?")
     task_name = input('> ')
     man.complete_item(task_name)
+
 else:
     print("sorry, the thing you said made no sense. The commands are:")
+    print('-' * 10)
     print("add - lets you add an item to the list. You will be asked what the name of the task you'd like to complete is")
+    print('-' * 10)
     print("list - lets you view your to-do list")
+    print('-' * 10)
     print("complete - lets you finish a task on your list. You will be asked what the name of the task you'd like to mark off is")
+    print('-' * 10)
